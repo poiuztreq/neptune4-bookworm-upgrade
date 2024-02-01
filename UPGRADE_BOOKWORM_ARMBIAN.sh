29115f5853129b086c1d43dd0555cc44a7ff1313
poiuztreq <harder.alex@gmx.net>
2024-02-01 10:40:44 +0100

    fixes

--- UPGRADE_BOOKWORM_ARMBIAN.sh
+++ UPGRADE_BOOKWORM_ARMBIAN.sh
@@ -6,15 +6,17 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 echo "Updating Buster repositories.."
-apt update && apt upgrade -y 
+apt update && apt upgrade -y > /dev/null
+apt autoremove > /dev/null
 
 echo "Patching Buster repositories to Bookworm and updating"
 sed -i '/^#deb-src/d' /etc/apt/sources.list
 sed -i '/^#deb http:\/\/security.debian.org\/ buster\/updates main contrib non-free/d' /etc/apt/sources.list
+sed -i 's/buster/bookworm/g' /etc/apt/sources.list.d/armbian.list
 sed -i 's/^deb/#deb/' /etc/apt/sources.list.d/armbian.list
-sed -i 's/bullseye/bookworm/' /etc/apt/sources.list
-echo "deb http://security.debian.org/ bookworm-security main contrib non-free" | sudo tee -a /etc/apt/sources.list
-apt update
+sed -i 's/bullseye/bookworm/g' /etc/apt/sources.list
+echo "deb http://security.debian.org/ bookworm-security main contrib non-free" | sudo tee -a /etc/apt/sources.list > /dev/null
+apt update > /dev/null
 
 echo "Upgrading already installed packages first"
 apt upgrade --without-new-pkgs -y
@@ -28,6 +30,8 @@ clear
 
 echo "Enabling Armbian repo and cleaning up"
 sed -i 's/^#deb/deb/' /etc/apt/sources.list.d/armbian.list
+echo 'APT::Get::Update::SourceListWarnings::NonFreeFirmware "false";' | tee /etc/apt/apt.conf.d/no-bookworm-firmware.conf
+
 apt autoremove
 
 echo -e "Rebooting system.\nYou have to run 'apt update && apt dist-upgrade' after reboot. Enjoy"
